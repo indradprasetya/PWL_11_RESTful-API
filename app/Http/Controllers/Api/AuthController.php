41bfa2e3c5d759dd7bb7be66b498b41ec2e573bc
indradprasetya <indrayanadian7@gmail.com>
2023-06-20 09:07:40 +0700

    logout done

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -51,4 +51,17 @@ class AuthController extends Controller
             'user' => $user
         ]);
     }
+
+    public function logout(Request $request)
+    {
+        try {
+            auth()->user()->tokens()->delete();
+            return $this->apiSuccess('Token Revoked');
+        } catch (\Throwable $e) {
+            throw new HttpResponseException($this->apiError(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            ));
+        }
+    }
 }
